dcdcbe7a214fcadc8ad2f1eef8bb073a77b06756
Sak <msak@luxoft.com>
2024-09-06 16:53:16 +0200

    working scenario, formated, updated

--- tic-tac-toe game/tic_tac_toe.py
+++ tic-tac-toe game/tic_tac_toe.py
@@ -1,6 +1,6 @@
 players = {}
-BOARD = ['_'] * 9
-PLAYER = None
+board = ['_'] * 9
+player = None
 
 
 def display_board(board):
@@ -48,21 +48,20 @@ def get_available_fields(board):
     return [str(i) for i, e in enumerate(board) if e == "_"]
 
 
-def ask_player_for_the_next_move(availible_fields, player, sign):
+def ask_for_next_move(available_fields, player, sign):
     next_move = None
-    while next_move not in availible_fields:
+    while next_move not in available_fields:
         next_move = input(f"{player}, where would you place {
                           sign} next from (0-8): ")
 
-        if next_move not in availible_fields:
-            print(f"Wrong, you have provided {
-                  next_move}, but availible are: {availible_fields}.")
+        if next_move not in available_fields:
+            print(f"Invalid move: {next_move}. Available moves are: {available_fields}.")
     return int(next_move)
 
 
 def check_win(move, board):
     move = int(move)
-    lista = [
+    winning_combinations = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -72,39 +71,28 @@ def check_win(move, board):
         [0, 4, 8],
         [2, 4, 6],
     ]
-    for l in lista:
+    for l in winning_combinations:
         if move in l:
-            print(l)
             if board[l[0]] == board[l[1]] == board[l[2]]:
-                print(f"{board[l[0]]} WINS")
                 return True
     return False
 
-
-# tests
-
-example0_board = ['_', '_', '_', '_', '_', '_', '_', '_', '_']
-example1_board = ['O', 'X', '_', 'O', 'X', '_', 'O', '_', '_']
-example2_board = ['O', 'X', 'X', 'O', 'X', '_', 'O', '_', '_']
-example3_board = ['O', '_', '_', 'X', '_', '_', 'O', '_', '_']
-
-BOARD = example0_board
 create_players()
 
-PLAYER = players['player1']
+player = players['player1']
 WIN = False
-for i in range(0, 9):
-    print(f"current player is {PLAYER['name']}")
-    display_board(BOARD)
-    available_fields = get_available_fields(BOARD)
-    next_move = int(ask_player_for_the_next_move(available_fields, PLAYER['name'], PLAYER['sign']))
-    BOARD[next_move] = PLAYER['sign']
-    WIN = check_win(next_move, BOARD)
-    print("WIN?:", WIN)
+display_board(board)
+for i in range(9):
+    available_fields = get_available_fields(board)
+    next_move = ask_for_next_move(available_fields, player['name'], player['sign'])
+    next_move = int(next_move)
+    board[next_move] = player['sign']
+    display_board(board)
+    WIN = check_win(next_move, board)
     if WIN:
-        print(f'{PLAYER['name']} has win. Congratulations!')
+        print(f'{player['name']} has win. Congratulations!')
         break
     else:
-        PLAYER = switch_player(PLAYER)
+        player = switch_player(player)
 else:
-    print("Draw")
+    print("It's a draw!")
