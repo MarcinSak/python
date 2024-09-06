bc9f1ec913cefd3a269a2f203a456c0d900deebd
Sak <msak@luxoft.com>
2024-09-06 14:54:17 +0200

    adding more functions

--- tic-tac-toe game/tic_tac_toe.py
+++ tic-tac-toe game/tic_tac_toe.py
@@ -1,3 +1,8 @@
+players = {}
+BOARD = ['_'] * 9
+PLAYER = None
+WINNER = None
+
 def display_board(board):
     spacing = "-"*11
     print(f"+{spacing}+")
@@ -8,47 +13,94 @@ def display_board(board):
     print(f"| {board[6]} | {board[7]} | {board[8]} |")
     print(f"+{spacing}+")
 
-def count_X_or_O_in_board_line(board):
-    occupied = 0
-    for i in range(3,9,3):
-        check = 3 - board[:i].count('')
-        if check > occupied:
-            occupied = check
-    return occupied
-
-def replace_in_list(to_be_replaced, replace_for, list_to_proceed):
-    new_list = list_to_proceed
-    for index, element in enumerate(list_to_proceed):
-        if element == to_be_replaced:
-            new_list[index] = replace_for
-    return new_list
-    
-
-example0_board = ['_','_','_','_','_','_','_','_','_']
-example1_board = ['O', 'X', '_', 'O', 'X', '_', 'O', '_', '_']
-example2_board = ['O', 'X', 'X', 'O', 'X', '_', 'O', '_', '_']
-example3_board = ['O', '_', '_', 'O', '_', '_', 'O', '_', '_']
-
-display_board(example0_board)
-
-
-players = {}
 
 def get_player_name(player_no):
     return input(f"Player{player_no} name: ")
+
 
 def get_player_sign():
     char = None
     accepted_chars = ['X', 'O']
     while char not in accepted_chars:
-        char = input("Which sigh do you prefer? Please choose X or O: ").upper()
+        char = input(
+            "Which sign do you prefer? Please choose X or O: ").upper()
         if char not in accepted_chars:
             print(f"Wrong, you have provided {char}, expected was X or O.")
     return char
 
-players["player1"] = {'name': f'{get_player_name(1)}', 'sign': f'{get_player_sign()}'}
-players["player2"] = {
-    'name': get_player_name(2),
-    'sign': 'O' if players["player1"]['sign'] == 'X' else 'X'
-}
+
+def create_players():
+    players["player1"] = {
+        'name': get_player_name(1),
+        'sign': get_player_sign()
+    }
+
+    players["player2"] = {
+        'name': get_player_name(2),
+        'sign': 'O' if players["player1"]['sign'] == 'X' else 'X'
+    }
+
+
+def switch_player(current_player):
+    return players['player2'] if current_player == players['player1'] else players['player1']
+
+
+
+def get_available_fields(board):
+    return [str(i) for i, e in enumerate(board) if e == "_"]
+
+
+def ask_player_for_the_next_move(availible_fields, player, sign):
+    next_move = None
+    while next_move not in availible_fields:
+        next_move = input(f"{player}, where would you place {sign} next from (0-8): ")
+
+        if next_move not in availible_fields:
+            print(f"Wrong, you have provided {next_move}, but availible are: {availible_fields}.")
+    return next_move
+
+
+def input_next_move(next_move, sign, board):
+    pass
+
+
+def check_win():
+    pass
+
+
+def present_result():
+    pass
+
+
+# tests
+
+example0_board = ['_', '_', '_', '_', '_', '_', '_', '_', '_']
+example1_board = ['O', 'X', '_', 'O', 'X', '_', 'O', '_', '_']
+example2_board = ['O', 'X', 'X', 'O', 'X', '_', 'O', '_', '_']
+example3_board = ['O', '_', '_', 'X', '_', '_', 'O', '_', '_']
+
+BOARD = example3_board
+create_players()
+display_board(BOARD)
+
+
+
+check_win()
+
+
 print(players)
+# display_board(example0_board)
+
+PLAYER =  players['player1']
+WIN = False
+while not WIN:
+    print("current player is:")
+    print(PLAYER)
+    display_board(BOARD)
+    available_fields = get_available_fields(BOARD)
+    next_move = ask_player_for_the_next_move(available_fields, PLAYER['name'], PLAYER['sign'])
+    BOARD[int(next_move)] = PLAYER['sign']
+    WIN = check_win(BOARD)
+    PLAYER = switch_player(PLAYER)
+
+present_result()
