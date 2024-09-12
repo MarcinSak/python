1842e2ed77baea5e80733d8c875ac51b291b09b6
Sak <msak@luxoft.com>
2024-09-12 18:20:46 +0200

    remove cards map

--- war card game/data.py
+++ war card game/data.py
@@ -1,9 +1,3 @@
 suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
 ranks = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Joker", "Queen", "King", "Ace"]
 values = {f"{rank}": f"{index+2}" for index, rank in enumerate(ranks)}
-
-cards = [
-    {"rank": rank, "suit": suit, "value": index + 2}
-    for index, rank in enumerate(ranks)
-    for suit in suits
-]
