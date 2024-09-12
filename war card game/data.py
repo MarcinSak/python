9518aa5bba1355768719cebf342f9b1acfadb054
Sak <msak@luxoft.com>
2024-09-12 19:36:18 +0200

    Refactor Data Module for Clearer Suit, Rank, and Value Definitions

--- war card game/data.py
+++ war card game/data.py
@@ -1,3 +1,9 @@
+"""
+Contains data structures related to the card game, such as the suits, ranks, and their 
+corresponding values. This module provides the foundational information for creating a 
+deck of cards.
+"""
+
 suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
 ranks = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Joker", "Queen", "King", "Ace"]
-values = {f"{rank}": f"{index+2}" for index, rank in enumerate(ranks)}
+values = {rank: index + 2 for index, rank in enumerate(ranks)}
