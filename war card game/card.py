58e39d8932aded891531f69b5a3cc8333a4ae038
Sak <msak@luxoft.com>
2024-09-12 18:23:35 +0200

    fix value to return int type

--- war card game/card.py
+++ war card game/card.py
@@ -1,11 +1,9 @@
 '''Assume 52-cards option'''
 import data
-#suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
-
 class Card():
     def __init__(self, rank: str, suit: str):
         self.rank = rank
         self.suit = suit
-        self.value = data.values[rank]
+        self.value = int(data.values[rank])
     def __str__(self) -> str:
         return f"{self.rank} of {self.suit} ({self.value})"
