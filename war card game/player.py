7020f4ed800a743db1d474990e9242d711f612cc
Sak <msak@luxoft.com>
2024-09-12 18:21:54 +0200

    fix logic remove_one method

--- war card game/player.py
+++ war card game/player.py
@@ -1,10 +1,12 @@
 import card
 class Player():
     def __init__(self, name: str):
+        print(f"Player {name} created")
         self.name = name
         self.all_cards = []
     def remove_one(self) -> card.Card:
-        self.all_cards.pop(0)
+        return self.all_cards.pop(0)
+        
     def add_one(self, card) -> None:
         if type(card) == type([]):
             self.all_cards.extend(card)
@@ -12,3 +14,5 @@ class Player():
             self.all_cards.append(card)
     def __str__(self) -> str:
         return f"Player: {self.name}, has {len(self.all_cards)} cards"
+    def __len__(self) -> int:
+        return len(self.all_cards)
