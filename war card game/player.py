e4f5e8912106b2ff7b05498639d682c481bb7e68
Sak <msak@luxoft.com>
2024-09-12 19:28:05 +0200

    Refactor Player Class for Improved Card Management and Data Consistency

--- war card game/player.py
+++ war card game/player.py
@@ -1,21 +1,25 @@
 import card
 from random import shuffle
 
-class Player():
+class Player:
     def __init__(self, name: str):
         print(f"Player {name} created")
         self.name = name
-        self.all_cards = []
+        self.all_cards: List[card.Card] = []
+
     def remove_one(self) -> card.Card:
         return self.all_cards.pop(0)
         
-    def add_cards(self, cards) -> None:
-        if type(cards) == type([]):
+    def add_cards(self, cards: list) -> None:
+        """Adds a card or a list of cards to the player's hand."""
+        if isinstance(cards, list):
             shuffle(cards)
             self.all_cards.extend(cards)
         else:
             self.all_cards.append(cards)
+
     def __str__(self) -> str:
         return f"Player: {self.name}, has {len(self)} cards"
+    
     def __len__(self) -> int:
         return len(self.all_cards)
